84051b576b8e07bcdd120c8cfcd04d185d229c09
Sneha Sinha <sneha16098@iiitd.ac.in>
2018-10-11 22:40:37 +0530

    final submission

--- app/src/main/java/com/example/sneha/quizme/CSVWriter.java
+++ app/src/main/java/com/example/sneha/quizme/CSVWriter.java
@@ -1,6 +1,8 @@
 package com.example.sneha.quizme;
 
 
+import android.text.TextUtils;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.Writer;
@@ -10,91 +12,23 @@ public class CSVWriter {
 
     private PrintWriter pp;
 
-    private char separator;
-
-    private char quotechar;
-
-    private char escapechar;
-
-    private String lineEnd;
-
-    public static final char DEFAULT_ESCAPE_CHARACTER = '"';
-
-    public static final char DEFAULT_SEPARATOR = ',';
-
-
-    public static final char DEFAULT_QUOTE_CHARACTER = '"';
-
-    public static final char NO_QUOTE_CHARACTER = '\u0000';
-
-    public static final char NO_ESCAPE_CHARACTER = '\u0000';
-
-    public static final String DEFAULT_LINE_END = "\n";
-
 
     public CSVWriter(Writer writer) {
-        this(writer, DEFAULT_SEPARATOR, DEFAULT_QUOTE_CHARACTER,
-                DEFAULT_ESCAPE_CHARACTER, DEFAULT_LINE_END);
-    }
-
-
-
-    public CSVWriter(Writer writer, char separator, char quotechar, char escapechar, String lineEnd) {
         this.pp = new PrintWriter(writer);
-        this.separator = separator;
-        this.quotechar = quotechar;
-        this.escapechar = escapechar;
-        this.lineEnd = lineEnd;
     }
+
 
 
     public void writeNext(String[] nextLine) {
+        String toWrite = TextUtils.join(",", nextLine);
+        pp.write(toWrite + "\n");
 
-        if (nextLine == null)
-            return;
-
-        StringBuffer sb = new StringBuffer();
-        for (int i = 0; i < nextLine.length; i++) {
-
-            if (i != 0) {
-                sb.append(separator);
-            }
-
-            String nextElement = nextLine[i];
-            if (nextElement == null)
-                continue;
-            if (quotechar !=  NO_QUOTE_CHARACTER)
-                sb.append(quotechar);
-            for (int j = 0; j < nextElement.length(); j++) {
-                char nextChar = nextElement.charAt(j);
-                if (escapechar != NO_ESCAPE_CHARACTER && nextChar == quotechar) {
-                    sb.append(escapechar).append(nextChar);
-                } else if (escapechar != NO_ESCAPE_CHARACTER && nextChar == escapechar) {
-                    sb.append(escapechar).append(nextChar);
-                } else {
-                    sb.append(nextChar);
-                }
-            }
-            if (quotechar != NO_QUOTE_CHARACTER)
-                sb.append(quotechar);
-        }
-
-        sb.append(lineEnd);
-        pp.write(sb.toString());
+        return ;
 
     }
 
-
-    public void flush() throws IOException {
-
-        pp.flush();
-
-    }
-
-
-    public void close() throws IOException {
+    public void close() {
         pp.flush();
         pp.close();
     }
-
 }
