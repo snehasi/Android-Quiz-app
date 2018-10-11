01a25de426141573094a8640a62c38e1f9dbb5b4
Sneha Sinha <sneha16098@iiitd.ac.in>
2018-10-11 10:47:15 +0530

    correct csv sequences for new answers and conflicts

--- app/src/main/java/com/example/sneha/quizme/DetailActivity.java
+++ app/src/main/java/com/example/sneha/quizme/DetailActivity.java
@@ -19,6 +19,9 @@ import java.io.File;
 import java.io.FileWriter;
 
 import static android.content.ContentValues.TAG;
+import static com.example.sneha.quizme.myDatabaseHelper.DATABASE_NAME;
+import static com.example.sneha.quizme.myDatabaseHelper.TABLE_ANS;
+import static com.example.sneha.quizme.myDatabaseHelper.TABLE_QUES;
 
 //third screen has been connected
 public class DetailActivity extends AppCompatActivity implements DetailFragment.OnFragmentInteractionListener {
@@ -94,11 +97,11 @@ public class DetailActivity extends AppCompatActivity implements DetailFragment.
     public void AddData() {
         Toast.makeText(this, "save", Toast.LENGTH_SHORT).show();
         if(xt==false){
-            databaseHelper.insertd("False");
+            databaseHelper.insertd(getst(), "False");
 
         }
         if(xt==true){
-            databaseHelper.insertd("True");
+            databaseHelper.insertd(getst(), "True");
         }
     }
 
@@ -131,18 +134,51 @@ public class DetailActivity extends AppCompatActivity implements DetailFragment.
             file.createNewFile(); //error here
             //file.setDestinationInExternalFilesDir(this, getFilesDir().toString(), "song_15.mp3");
             Log.d("checklocation", file.getAbsolutePath());
+
+
             CSVWriter csvWrite = new CSVWriter(new FileWriter(file));
+
+
             SQLiteDatabase db = dbh.getReadableDatabase();
-            Cursor curCSV = db.rawQuery("SELECT * FROM savedanswer",null);
-            csvWrite.writeNext(curCSV.getColumnNames());
-            while(curCSV.moveToNext())
-            {
-                //Which column you want to exprort
-                String arrStr[] ={curCSV.getString(0),curCSV.getString(1), curCSV.getString(2)};
-                csvWrite.writeNext(arrStr);
+
+            Cursor curCSV = db.rawQuery("SELECT * FROM "+ TABLE_ANS ,null);
+            if(curCSV.moveToFirst()){
+                csvWrite.writeNext(curCSV.getColumnNames());
+
+                do{
+                    //Which column you want to exprort
+                    String arrStr[] ={curCSV.getString(0),curCSV.getString(1)};
+                    csvWrite.writeNext(arrStr);
+                }while(curCSV.moveToNext());
+                csvWrite.close();
+                curCSV.close();
             }
-            csvWrite.close();
-            curCSV.close();
+
+//            Cursor curCSV2 = db.rawQuery("SELECT * FROM "+ TABLE_ANS ,null);
+//            if(curCSV2.moveToFirst()){
+//                csvWrite.writeNext(curCSV2.getColumnNames());
+//
+//                do{
+//                    //Which column you want to exprort
+//                    String arrStr2[] ={curCSV2.getString(0),curCSV2.getString(1)};
+//
+//                    csvWrite.writeNext(arrStr2);
+//                }while(curCSV2.moveToNext());
+//                csvWrite.close();
+//                curCSV2.close();
+//            }
+
+
+
+
+//            if (curCSV ==null) {
+//                Log.d("whateven", "null query from db");
+//            }
+//            else{
+//                Log.d("whateven", "not null query from db");
+//            }
+
+
         }
         catch(Exception sqlEx)
         {
--- app/src/main/java/com/example/sneha/quizme/myDatabaseHelper.java
+++ app/src/main/java/com/example/sneha/quizme/myDatabaseHelper.java
@@ -14,6 +14,7 @@ public class myDatabaseHelper extends SQLiteOpenHelper {
     public static final String DATABASE_NAME="questions.db";
     public static final String TABLE_QUES="question";
     public static final String TABLE_ANS="savedanswer";
+    private static final int DATABASE_VERSION = 2;
 
     //col for q
     public static final String COL_QUES_1="ID";
@@ -27,14 +28,14 @@ public class myDatabaseHelper extends SQLiteOpenHelper {
 
 
     public myDatabaseHelper(Context context) {
-        super(context, DATABASE_NAME, null, 1);
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
         this.context = context;
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE " +TABLE_QUES+ "(ID INTEGER PRIMARY KEY AUTOINCREMENT, ques TEXT)");
-        db.execSQL("CREATE TABLE " +TABLE_ANS+ "(ID INTEGER PRIMARY KEY AUTOINCREMENT, ans TEXT)");
+        db.execSQL("CREATE TABLE " +TABLE_QUES+ "(ID INTEGER PRIMARY KEY ON CONFLICT REPLACE , ques TEXT)");
+        db.execSQL("CREATE TABLE " +TABLE_ANS+ "(ID INTEGER PRIMARY KEY ON CONFLICT REPLACE , ans TEXT)");
 
         ContentValues val = new ContentValues();
         Resources r = context.getResources();
@@ -55,17 +56,15 @@ public class myDatabaseHelper extends SQLiteOpenHelper {
 
     }
 
-    public boolean insertd(String choice) {
+    public void insertd(int pos, String choice) {
+        //DetailActivity da = new DetailActivity();
+        //int x=da.getst();
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues cv = new ContentValues();
+        cv.put("ID", pos);
         cv.put("ans",choice);
-        long result = db.insert(TABLE_ANS,null,cv);
-        if(result ==-1){
-            return false;
-        }
-        else{
-            return true;
-        }
+        db.insert(TABLE_ANS,null,cv);
+
 
 
     }
